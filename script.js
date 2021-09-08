0772268afb106f0f2301712f5f6ef48a9637cbd6
Ewoud Kuijpers <74767424+Ewaudtkuijpers@users.noreply.github.com>
2021-09-08 10:52:03 +0000

    bounce tegen 2 muren

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var x, y, v;
 
 function setup() {
-  createCanvas(500,400);
+  createCanvas(500, 500);
 
   x = 0;
   y = 200;
-  v = 5;
+  v = 3;
 }
 
 function draw() {
@@ -13,8 +13,12 @@ function draw() {
 
   ellipse(x,y,50,50);
   x = x + v;
+  y = y + v;
 
   if(x <= 0 || x > 500){
     v = v * -1;
+  }
+  if(y > 500 || y <= 0){
+    v = v *-1;
   }
 }
