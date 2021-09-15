3b8bb8cd3cf2dd5ad2e018db06ddfbb05f52b120
Ewoud Kuijpers <74767424+Ewaudtkuijpers@users.noreply.github.com>
2021-09-15 10:51:06 +0000

    cunstructor bijna af

--- script.js
+++ script.js
@@ -7,13 +7,26 @@ class Ball{
     this.vx = vx;
     this.vy = vy;
   }
+  drawBall(){
+  ellipse(this.x,this.y,this.w,this.h);
+  this.x = this.x + this.vx;
+  this.y = this.y + this.vy;
+
+  if(this.x <= 0 || this.x > 500){
+    this.vx = this.vx * -1;
+  }
+  if(this.y > 500 || this.y <= 0){
+    this.vy = this.vy *-1;
+  }
 }
 
 
-function setup() {
+function setup(){
   createCanvas(500, 500);
+  background(205);
 }
 
-function draw() {
-  background(225);
+
+function draw(){
+ ball1 = new Ball(0,200,20,20,5,5);
 }
